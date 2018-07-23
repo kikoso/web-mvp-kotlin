41770264646394b37395e7a9014013a35532b2c3
Enrique Lopez Manas <eenriquelopez@gmail.com>
2018-07-23 16:49:37 +0700

    implementation in Gradle file

--- src/test/kotlin/com/kotlinmvpweb/demo/HelloWorldTest.kt
+++ src/test/kotlin/com/kotlinmvpweb/demo/HelloWorldTest.kt
@@ -1,6 +1,6 @@
-
 import mu.KotlinLogging
 import org.junit.After
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import kotlin.test.assertTrue
@@ -15,12 +15,17 @@ class ExampleTests {
     }
 
     @Test
-    fun exampleTest() {
+    fun exampleTestInline() {
         logger.debug { "We are executing our test" }
         assertTrue {
             logger.debug { "Here an assertion will take place" }
             true
         }
+    }
+
+    @Test
+    fun testOk() {
+        Assert.assertEquals("A string", "A different string" )
     }
 
     @After
